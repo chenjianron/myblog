6e1eaf0eae23029b1d34a8bbde8d84e9c31a89e8
chenjianrong <2440398853@qq.com>
2020-02-05 16:32:00 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+     "extends": "standard",
+     "globals": {
+       "describe": true,
+       "beforeEach": true,
+       "afterEach": true,
+       "after": true,
+       "it": true
+      },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
---
+++ middlewares/check.js
@@ -0,0 +1,17 @@
+module.exports = {
+  checkLogin: function checkLogin (req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录')
+      return res.redirect('/signin')  //将发出请求的用户代理重定向到给定的绝对或相对URL。
+    }
+    next()
+  },
+
+  checkNotLogin: function checkNotLogin (req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录')
+      return res.redirect('back')// 返回之前的页面
+    }
+    next()
+  }
+}
